85b7fc87ec496d9046f8e762bc99c2236e32618a
Aniruddh <aniruddhupparna@gmail.com>
2018-03-14 20:12:19 +0530

    added json-merge task for techstack files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
+var jsonConcat = require('gulp-json-concat');
+
 gulp.task("scripts", () => {
   gulp
     .src(["app/*.js"])
@@ -17,6 +19,17 @@ gulp.task("copy", () => {
   gulp.src("app/*.css").pipe(gulp.dest("dist"));
   gulp.src("app/*.html").pipe(gulp.dest("dist"));
   gulp.src("app/assets/**/*").pipe(gulp.dest("dist/assets"));
+});
+
+// concat JSON files
+gulp.task('merge-json', () => {
+  gulp.src('techstack/*.json')
+    .pipe(jsonConcat('radar-data.json', function (data) {
+      var finalObj = {};
+      finalObj.entries = Object.values(data);
+      return new Buffer(JSON.stringify(finalObj, null, '\t'));
+    }))
+    .pipe(gulp.dest('data/'));
 });
 
 gulp.task("copy-data", () => {
